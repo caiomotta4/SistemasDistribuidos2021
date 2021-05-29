ee8cb7b89ce70355361b87e28f209c33249674d8
caiomotta4 <62080324+caiomotta4@users.noreply.github.com>
2021-05-29 00:28:35 -0300

    .

---
+++ Avaliacao/src/main/java/br/com/MyApplication.java
@@ -0,0 +1,13 @@
+package br.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
